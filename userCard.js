857205e8a2a6ccd696b677cbde276f8882e69f31
PrathamMishra <55537618+PrathamMishra@users.noreply.github.com>
2022-02-25 12:56:35 +0530

    Shadow Dom Added

---
+++ userCard.js
@@ -0,0 +1,12 @@
+const template = document.createElement('template');
+template.innerHTML=`<h1>Sample heading</h1>`
+
+class UserCard extends HTMLElement{
+    constructor(){
+        super();
+        this.attachShadow({mode:"open"})
+        this.shadowRoot.appendChild(template.content.cloneNode(true))
+    }
+}
+
+customElements.define('user-card',UserCard)
